c8500f6916e456649cbcc2786c94d9a3dc27cc0a
Alexander Malyshev <a.san.malyshev@mail.ru>
2021-02-28 12:02:10 +0300

    add multi-grammar and -rule config

--- scripts/decoder.py
+++ scripts/decoder.py
@@ -25,9 +25,11 @@ class VoiceDecoder(object):
         rospy.spin()
 
     def load_config(self):
-        self._hmm = rospy.get_param("~hmm", False)
-        self._dict = rospy.get_param("~dict", False)
-        self._kws = rospy.get_param("~kws", False)
+        self._hmm = rospy.get_param("~hmm", None)
+        self._dict = rospy.get_param("~dict", None)
+
+        self._kws = rospy.get_param("~kws", None)
+
         self._keyphrase = rospy.get_param("~keyphrase", False)
         self._threshold = rospy.get_param("~threshold", False)
         self._gram = rospy.get_param("~gram", False)
@@ -35,33 +37,51 @@ class VoiceDecoder(object):
         self._rule = rospy.get_param("~rule", False)
         #self._out_topic = rospy.get_param("~out_topic", "decoded_phrase")
 
-        rospy.logwarn("Using default hmm") if not self._hmm else rospy.loginfo("hmm file: %s", self._hmm)
-        rospy.logwarn("Using default dict") if not self._dict else rospy.loginfo("Dict file: %s", self._dict)
 
     def start_recognizer(self):
 
         config = pocketsphinx.Decoder.default_config()
 
         # Setup decoder config
-        if self._hmm : config.set_string('-hmm', self._hmm)
-        if self._dict : config.set_string('-dict', self._dict)
+        if self._hmm is None: 
+            rospy.logwarn("Using default hmm") 
+        else:
+            rospy.loginfo("hmm file: %s", self._hmm)
+            config.set_string('-hmm', self._hmm)
+
+        if self._dict is None: 
+            rospy.logwarn("Using default dict")
+        else:
+            rospy.loginfo("Dict file: %s", self._dict)
+            config.set_string('-dict', self._dict)
+
         config.set_string('-dither', "no")
         config.set_string('-featparams', os.path.join(self._hmm, "feat.params"))
         #config.set_boolean('-bestpath', True)
-        if self._kws: config.set_string('-kws', self._kws)
-        # else:
-        #     # In case keyphrase is provided
-        #     config.set_string('-keyphrase', self.keyphrase)
-        #     config.set_float('-kws_threshold', self.kws_threshold)
+        if self._kws is not None: 
+            config.set_string('-kws', self._kws)
+            if self._keyphrase is not None: config.set_string('-keyphrase', self.keyphrase)
+            if self._threshold is not None: config.set_float('-kws_threshold', self.kws_threshold)
 
         # Set required configuration for decoder
         self.decoder = pocketsphinx.Decoder(config)
 
         if self._gram and self._grammar and self._rule:
             jsgf = Jsgf(self._gram)
-            rule = jsgf.get_rule(self._grammar + '.' + self._rule)
-            fsg = jsgf.build_fsg(rule, self.decoder.get_logmath(), 7.5)
-            fsg.writefile(self._gram + '.fsg')
+            if isinstance(self._rule,str):
+                self._rule = [self._rule]
+            if isinstance(self._grammar,str):
+                self._grammar = [self._grammar]
+            for grammar in self._grammar:
+                for  r in self._rule:
+                    rule = jsgf.get_rule(grammar + '.' + r)
+                    # rospy.logwarn(rule)
+                    if rule is not None:
+                        fsg = jsgf.build_fsg(rule, self.decoder.get_logmath(), 7.5)
+                        fsg.writefile(self._gram + '.fsg')
+                        rospy.logwarn("Rule <"+ r + "> from grammar <" + grammar + "> is loaded")
+                    else:
+                        rospy.logwarn("No rule <"+ r + "> in grammar <" + grammar + ">")
 
             self.decoder.set_fsg(self._gram, fsg)
             self.decoder.set_search(self._gram)
